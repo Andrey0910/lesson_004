549c7099a0cd0bd9461bc67586a84c9036139abf
Andrey Petrov <petrovaa71@mail.ru>
2018-03-20 12:55:56 +0300

    поправил фориатирование кода

--- functions.php
+++ functions.php
@@ -1,16 +1,20 @@
 <?php
-interface CarInterface{
+
+interface CarInterface
+{
     public function go($distance);
 }
+
 class Car implements CarInterface
 {
     public $distance;
     public $speed;
     private $way;
-    public  $direction;
+    public $direction;
     public $transmission;
     public $season;
     use Engine, AutomaticTransmission, ManualTransmission, Wheels;
+
     public function __construct($distance, $speed, $way, $transmission, $season)
     {
         $this->distance = $distance;
@@ -18,88 +22,111 @@ class Car implements CarInterface
         $this->way = $way;
         $this->transmission = $transmission;
         $this->season = $season;
-        echo "Расстояние - ".$this->distance;
-        echo "<br>", "Скорость - ".$this->speed;
-        echo "<br>", "Коробка передач - ". $this->transmission;
-        echo "<br>",  $this->rubber($season);
+        echo "Расстояние - " . $this->distance;
+        echo "<br>", "Скорость - " . $this->speed;
+        echo "<br>", "Коробка передач - " . $this->transmission;
+        echo "<br>", $this->rubber($season);
     }
-    public function start(){
+
+    public function start()
+    {
         $this->on();
     }
-    public function onWay(){
-        if($this->transmission == "automatic"){
+
+    public function onWay()
+    {
+        if ($this->transmission == "automatic") {
             $this->driveAutomatic();
-        }
-        elseif($this->transmission == "manual"){
+        } elseif ($this->transmission == "manual") {
             $this->driveManual($this->speed);
         }
     }
+
     public function go($distance)
     {
-        for ($i=0, $t=0; $i < $this->distance; $i+=10, $t+=5){
-            if ($t == 90){
-            echo "Температурп двигателя - ".$t.". ";
-            $t = $this->cooling($t);
-            echo "<br>";
+        for ($i = 0, $t = 0; $i < $this->distance; $i += 10, $t += 5) {
+            if ($t == 90) {
+                echo "Температурп двигателя - " . $t . ". ";
+                $t = $this->cooling($t);
+                echo "<br>";
             }
         }
     }
-    public function stop(){
+
+    public function stop()
+    {
         $this->off();
     }
 }
-trait Engine{
-    public function on(){
+
+trait Engine
+{
+    public function on()
+    {
         echo "<br>", "Включил двигатель.";
     }
-    public function off(){
+
+    public function off()
+    {
         echo "Выключил двигатель.";
     }
-    public function cooling($t){
+
+    public function cooling($t)
+    {
         echo "Включилось охлождение двигателя";
         return $t - 10;
     }
 }
-class Car3{
+
+class Car3
+{
     public $speed;
     public $transmission;
+
     public function __construct($speed, $transmission)
     {
         $this->speed = $speed;
         $this->transmission = $transmission;
     }
 }
-trait AutomaticTransmission{
+
+trait AutomaticTransmission
+{
     public function driveAutomatic()
     {
         echo "Включил на каробке передач режим езды в перед.";
     }
+
     public function reverseAutomatic()
     {
         echo "Включил на каробке передач режим езды назад.";
     }
 }
-trait ManualTransmission{
+
+trait ManualTransmission
+{
     public function driveManual($speed)
     {
-        if ($speed <= 20){
+        if ($speed <= 20) {
             echo "Включил на каробке передач первая передача.";
-        }
-        else{
+        } else {
             echo "Включил на каробке передач вторая передача.";
         }
     }
+
     public function reverseManual()
     {
         echo "Включил на каробке передач задняя передача.";
     }
 }
-trait Wheels{
-    public function rubber($season){
-        if ($season == "winter"){
+
+trait Wheels
+{
+    public function rubber($season)
+    {
+        if ($season == "winter") {
             echo "Одета зимняя резина.";
-        }
-        else {
+        } else {
             echo "Одета леняя резина.";
         }
     }
